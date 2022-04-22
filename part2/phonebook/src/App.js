4e30103fddc3e4709a97da993ea0936102e58a59
Daniel <daniel.campos@picpay.com>
2022-04-22 12:25:59 -0300

    part2: phonebook step 6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,19 +1,24 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Persons from "./components/Persons";
 import Filter from "./components/Filter";
 import PersonsForm from "./components/PersonsForm";
+import axios from "axios";
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]);
+  const [persons, setPersons] = useState([]);
   const [shownPersons, setShownPersons] = useState(persons)
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data);
+        setShownPersons(response.data);
+      })
+  },[])
 
   const handleNameChange = (event) => {
     setNewName(event.target.value);

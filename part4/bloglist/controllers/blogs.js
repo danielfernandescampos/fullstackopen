1b4b35590c5430ab9213eab1ba4fe4a54c555f71
Daniel <daniel.campos@picpay.com>
2022-06-03 18:21:23 -0300

    part4: add more tests and organize with describes

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,19 +1,37 @@
 const blogsRouter = require("express").Router();
 const Blog = require("../models/blogs");
 
-blogsRouter.get("/", async (request, response, next) => {
+blogsRouter.get("/", async (request, response) => {
   const blogs = await Blog.find({});
   response.json(blogs);
 });
 
-blogsRouter.post("/", async (request, response, next) => {
+blogsRouter.post("/", async (request, response) => {
   let newBlog = request.body;
   if (!request.body.likes) newBlog = { ...newBlog, likes: 0 };
   if (!newBlog.url || !newBlog.title)
     return response.status(400).json({ error: "missing properties" });
   const blog = new Blog(newBlog);
-  const savedBlog = await blog.save();
-  response.status(201).json(savedBlog);
+  const savedBlog = await blog.save()
+  response.status(201).json(savedBlog)
 });
+
+blogsRouter.delete("/:id", async (request, response) => {
+  const id = request.params.id
+  await Blog.findByIdAndRemove(id)
+  response.status(204).end()
+})
+
+blogsRouter.put("/:id", async (request, response) => {
+  const id = request.params.id
+  const blog = request.body
+  const updatedBlog = await Blog.findByIdAndUpdate(id, blog, {
+    new: true,
+    runValidators: true,
+    context: 'query',
+  })
+  response.json(updatedBlog)
+
+})
 
 module.exports = blogsRouter;
--- part4/bloglist/tests/blog-api.test.js
+++ part4/bloglist/tests/blog-api.test.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const supertest = require("supertest");
 const app = require("../app");
 const Blog = require("../models/blogs");
-
 const api = supertest(app);
 
 const initialBlogs = [
@@ -41,65 +40,97 @@ test("blogs are returned as json", async () => {
     .expect("Content-Type", /application\/json/);
 }, 10000);
 
-test("all blogs are returned", async () => {
-  const response = await api.get("/api/blogs/");
-  expect(response.body).toHaveLength(initialBlogs.length);
-});
+describe("when there is initially some blogs saved", () => {
+  test("all blogs are returned", async () => {
+    const response = await api.get("/api/blogs/");
+    expect(response.body).toHaveLength(initialBlogs.length);
+  });
+  
+  test("a specific blog is within the returned blogs", async () => {
+    const response = await api.get("/api/blogs/");
+    const titles = response.body.map((r) => r.title);
+    expect(titles).toContain("Go To Statement Considered Harmful");
+  });
+  
+  test("the unique identifier of a blog is named id", async () => {
+    const response = await api.get("/api/blogs/");
+    response.body.map((blog) => expect(blog.id).toBeDefined());
+  });
+})
 
-test("a specific blog is within the returned blogs", async () => {
-  const response = await api.get("/api/blogs/");
-  const titles = response.body.map((r) => r.title);
-  expect(titles).toContain("Go To Statement Considered Harmful");
-});
+describe("addition of a new blog", () => {
+  test("verify that HTTP POST creates a blog", async () => {
+    const mockBlog = {
+      title: "React attack",
+      author: "Bruce Lee",
+      url: "sdhfdiosufjds",
+      likes: 99,
+    };
+    await api
+      .post("/api/blogs/")
+      .send(mockBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+  
+    const response = await api.get("/api/blogs");
+    const contents = response.body.map((r) => r.title);
+    expect(response.body.length).toBe(initialBlogs.length + 1);
+    expect(contents).toContain(mockBlog.title);
+  });
 
-test("the unique identifier of a blog is named id", async () => {
-  const response = await api.get("/api/blogs/");
-  response.body.map((blog) => expect(blog.id).toBeDefined());
-});
+  test("if the likes property is missing from request, the default should be 0", async () => {
+    const mockBlog = {
+      title: "React attack",
+      author: "Bruce Lee",
+      url: "sdhfdiosufjds",
+    };
+    const response = await api
+      .post("/api/blogs/")
+      .send(mockBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
+  
+    expect(response.body.likes).toBe(0);
+  });
+  test("should return 400 if title or url properties are missing at post request", async () => {
+    const mockBlog = {
+      title: "React attack",
+      author: "Bruce Lee",
+    };
+    await api.post("/api/blogs/").send(mockBlog).expect(400)
+    const response = await api.get("/api/blogs/")
+    expect(response.body).toHaveLength(initialBlogs.length)
+  });
+})
 
-test("verify that HTTP POST creates a blog", async () => {
-  const mockBlog = {
-    title: "React attack",
-    author: "Bruce Lee",
-    url: "sdhfdiosufjds",
-    likes: 99,
-  };
-  await api
-    .post("/api/blogs/")
-    .send(mockBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+describe("deletion of a blog", () => {
+  test("delete a blog with the id", async () => {
+    const response = await api.get("/api/blogs")
+    await api
+      .delete(`/api/blogs/${response.body[0].id}`)
+      .expect(204)
+    const responseDeleted = await api.get("/api/blogs") 
+    expect(responseDeleted.body.length).toBe(initialBlogs.length - 1)
+  })
+})
 
-  const response = await api.get("/api/blogs");
-  const contents = response.body.map((r) => r.title);
-  expect(response.body.length).toBe(initialBlogs.length + 1);
-  expect(contents).toContain(mockBlog.title);
-});
-
-test("if the likes property is missing from request, the default should be 0", async () => {
-  const mockBlog = {
-    title: "React attack",
-    author: "Bruce Lee",
-    url: "sdhfdiosufjds",
-  };
-  const response = await api
-    .post("/api/blogs/")
-    .send(mockBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
-
-  expect(response.body.likes).toBe(0);
-});
-
-test("should return 400 if title or url properties are missing at post request", async () => {
-  const mockBlog = {
-    title: "React attack",
-    author: "Bruce Lee",
-  };
-  await api.post("/api/blogs/").send(mockBlog).expect(400)
-  const response = await api.get("/api/blogs/")
-  expect(response.body).toHaveLength(initialBlogs.length)
-});
+describe("updates a blog", () => {
+  test("update the number of likes of a blog", async () => {
+    const updatedBlog = {
+      title: "teste",
+      author: "teste",
+      url: "teste",
+      likes: 99
+    }
+    const response = await api.get("/api/blogs")
+    await api
+      .put(`/api/blogs/${response.body[0].id}`)
+      .send(updatedBlog)
+      .expect(200)
+    const updatedResponse = await api.get("/api/blogs") 
+    expect(updatedResponse.body[0].likes).toBe(updatedBlog.likes)
+  })
+})
 
 afterAll(() => {
   mongoose.connection.close();

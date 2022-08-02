42c0427ed7ac654a1298863f718a3a14b07a8eaa
Daniel <daniel.campos@picpay.com>
2022-08-02 18:07:19 -0500

    part4: adjustments for tests

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -23,7 +23,14 @@ app.use(tokenExtractor)
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
+
+if(process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+}
+
 app.use(unknownEndpoint)
 app.use(errorHandler)
+
 
 module.exports = app
